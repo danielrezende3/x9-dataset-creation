e5a0fe30fc8e994416ad03a5ba762a4dc881939d
Daniel Rezende <danielrezende2@academico.ufs.br>
2025-01-29 12:44:38 -0300

    Refactor utils.py to improve error logging and file retrieval; remove exclusion list for files

--- scripts/utils.py
+++ scripts/utils.py
@@ -4,27 +4,24 @@ from pathlib import Path
 import re
 from typing import NoReturn
 
-EXCLUDE_FROM_LIST = ["problem_145.py", "problem_187.py"]
-
 
 def log_error(file_name, message: str) -> NoReturn:
+    """Logs an error message to the console and exits the program."""
+
     print(f"{file_name}: error: {message}")
     exit(1)
 
 
 def get_files_from_folder(folder: Path) -> list[Path]:
+    """Get a list of files in a folder"""
     return sorted(
-        [
-            Path(folder) / f
-            for f in listdir(folder)
-            if isfile(join(folder, f)) and f not in EXCLUDE_FROM_LIST
-        ],
+        [Path(folder) / f for f in listdir(folder) if isfile(join(folder, f))],
         key=natural_key,  # Use a custom sorting key
     )
 
 
-def natural_key(file_path: Path):
-    # Extract numeric parts of the file name for sorting
+def natural_key(file_path: Path) -> list[str | int]:
+    """Extract numeric parts of the file name for sorting"""
     return [
         int(text) if text.isdigit() else text
         for text in re.split(r"(\d+)", file_path.name)
